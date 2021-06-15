fec30e39b660e5caab0d61f394976ada59050611
javier ferrales <ferralespereira@gmail.com>
2021-06-15 15:11:50 -0400

    finishing middleware

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -1,6 +1,41 @@
 'use strict'
 
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var secret = "clave-secreta-para-generar-token-4411313aasnbxa";
+
 exports.authenticated = function(req, res, next){
-    console.log("estas en el middleware");
+
+    // comprobar si nos llega autorizacion
+    if(!req.headers.authorization){
+        return res.status(403).send({
+            message: 'No ha enviado el token'
+        });
+    }
+
+    // limpiar el token y quitar comillas
+    var token = req.headers.authorization.replace(/['"]+/g, '');
+
+    try{
+      // decodificar el token
+      var payload = jwt.decode(token, secret);
+
+      // comprobar si el token ha expirado (comparo la fecha del token con la fecha actual)
+      if(payload.exp <= moment.unix()){
+        return res.status(404).send({
+            message: 'El token ha expirado'
+        });
+      }
+
+    }catch(ex){
+      return res.status(404).send({
+          message: 'El token no es valido'
+      });
+    }
+
+    // adjunar usuario identificado a la request
+    req.user = payload;
+
+    // pasar a la accion
     next();
 };
