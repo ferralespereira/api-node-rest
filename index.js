bef8d573cfad62a26cff215fede6940f9f6bcb07
javier ferrales <ferralespereira@gmail.com>
2021-06-15 18:00:27 -0400

    fixing node errors

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 3999;
 
+// corrigiendo error de consola
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
 // conecto el proyecto a la base de datos
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/api-rest-node', { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost:27017/api-rest-node', { useNewUrlParser: true})
         .then(() => {
           console.log('La conexion a la base de datos de mongo se ha reaizado correctamente!')
           // Crear el servidor
