6cd6a0c4962b9a38172b1bafdaaf63b3cdb92cde
javier ferrales <ferralespereira@gmail.com>
2021-07-08 16:50:51 -0400

    detalles

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/api-rest-node', { useNewUrlParser: true})
         .then(() => {
-          console.log('La conexion a la base de datos de mongo se ha reaizado correctamente!')
+          console.log('La conexion a la base de datos de mongo se ha realizado correctamente!')
           // Crear el servidor
           app.listen(port, () => {
               console.log("El servidor HTTP://localhost:3999 esta funcionando");
