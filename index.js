c96e3c2cbd828edad6b6b0603d59481d20dd5cf0
javier ferrales <ferralespereira@gmail.com>
2022-04-19 10:23:55 -0400

    more

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = process.env.PORT || 3999;
+
+// adquiero el valor PORT del archivo .env
+require('dotenv').config();
+// var port = process.env.PORT || 3999;
+const port = process.env.PORT;
 
 // corrigiendo error de consola
 mongoose.set('useUnifiedTopology', true);
@@ -14,7 +18,7 @@ mongoose.connect('mongodb://localhost:27017/api-rest-node', { useNewUrlParser: t
           console.log('La conexion a la base de datos de mongo se ha realizado correctamente!')
           // Crear el servidor
           app.listen(port, () => {
-              console.log("El servidor HTTP://localhost:3999 esta funcionando");
+              console.log(`El servidor HTTP://localhost:${port} esta funcionando`);
           });
         })
         .catch(error => console.log(error));
